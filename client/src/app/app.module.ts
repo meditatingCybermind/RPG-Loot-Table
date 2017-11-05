158eae4aa5279a129442ae19b14c7eb78650fe74
meditatingCybermind <thomaschparsons@gmail.com>
2017-11-05 00:55:10 -0400

    upgraded to angular 5

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,22 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { HeroDetailComponent } from './hero/hero-detail/hero-detail.component';
+import { MainModule } from '../modules/main/main.module';
+// import { MaterialModule } from '@angular/material';
+// https://codeburst.io/angular-4-material-design-7b6975734ed6
+// https://material.angular.io/guide/getting-started
+// Material should be supported by Monday 11-6
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroDetailComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    HttpModule
+    MainModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
